e5d9111074b33d7ed79d915a814a88ff4e418cf0
RayzoRay <redbullslurpie@gmail.com>
2022-12-17 20:25:45 -0500

    Delete src directory

--- src/main/java/com/rbs/slurpiesdongles/common/items/tools/IBreakValidator.java
+++
@@ -1,8 +0,0 @@
-package com.rbs.slurpiesdongles.common.items.tools;
-
-import net.minecraft.world.level.block.state.BlockState;
-
-public interface IBreakValidator {
-    boolean canBreak(BlockState state);
-
-}
