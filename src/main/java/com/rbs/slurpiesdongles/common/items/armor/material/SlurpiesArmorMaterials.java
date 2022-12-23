af73e8aa55465b3983a26cb4f0e2bae4ae72c96c
RayzoRay <RedBullSlurpie@gmail.com>
2022-12-23 18:53:29 -0500

    Changes

--- src/main/java/com/rbs/slurpiesdongles/common/items/armor/material/SlurpiesArmorMaterials.java
+++ src/main/java/com/rbs/slurpiesdongles/common/items/armor/material/SlurpiesArmorMaterials.java
@@ -3,6 +3,7 @@ package com.rbs.slurpiesdongles.common.items.armor.material;
 import com.rbs.slurpiesdongles.core.init.ModItems;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.LazyLoadedValue;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Items;
@@ -11,69 +12,64 @@ import net.minecraft.world.item.crafting.Ingredient;
 import java.util.function.Supplier;
 
 public enum SlurpiesArmorMaterials implements ArmorMaterial {
-    AMETHYST ("slurpiesdongles:amethyst", 34, new int[]{3, 6, 7, 4}, 10, SoundEvents.ARMOR_EQUIP_DIAMOND,2.0F, 0F, () -> Ingredient.of(Items.AMETHYST_SHARD)),
-    RUBY("slurpiesdongles:ruby", 16, new int[]{2, 6, 6, 2}, 9, SoundEvents.ARMOR_EQUIP_DIAMOND, 0.0F, 0F, () -> Ingredient.of(ModItems.RUBY.get())),
-    TOPAZ("slurpiesdongles:topaz", 35, new int[]{4, 6, 8, 4}, 20, SoundEvents.ARMOR_EQUIP_DIAMOND, 2.0F, 0F, () -> Ingredient.of(ModItems.TOPAZ.get())),
-    WITHERED("slurpiesdongles:withered", 0, new int[]{4, 6, 7, 4}, 25, SoundEvents.ARMOR_EQUIP_DIAMOND, 2.0F, 0F, () -> Ingredient.of(Items.COAL));
+    AMETHYST ("raysdongles:amethyst", 35, new int[]{3, 6, 8, 3}, 13, SoundEvents.ARMOR_EQUIP_DIAMOND,2.5F, 0F, () -> Ingredient.of(Items.AMETHYST_SHARD)),
+    RUBY("raysdongles:ruby", 17, new int[]{2, 6, 6, 3}, 10, SoundEvents.ARMOR_EQUIP_IRON, 0.0F, 0F, () -> Ingredient.of(ModItems.RUBY.get())),
+    TOPAZ("raysdongles:topaz", 33, new int[]{3, 6, 8, 3}, 11, SoundEvents.ARMOR_EQUIP_DIAMOND, 2.0F, 0F, () -> Ingredient.of(ModItems.TOPAZ.get())),
+    WITHERED("raysdongles:withered", 0, new int[]{4, 7, 9, 4}, 15, SoundEvents.ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F, () -> Ingredient.of());
 
-    private static final int[] HEALTH_PER_SLOT = { 13, 15, 16, 11 };
+    private static final int[] HEALTH_PER_SLOT = new int[]{13, 15, 16, 11};
     private final String name;
     private final int durabilityMultiplier;
-    private final int[] armorVal;
-    private final int enchantability;
-    private final SoundEvent equipSound;
+    private final int[] slotProtections;
+    private final int enchantmentValue;
+    private final SoundEvent sound;
     private final float toughness;
     private final float knockbackResistance;
-    private final Ingredient repairIngredient;
+    private final LazyLoadedValue<Ingredient> repairIngredient;
 
-    SlurpiesArmorMaterials(String name, int durabilityMultiplier, int[] armorVal, int enchantability, SoundEvent equipSound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
-        this.name = name;
-        this.durabilityMultiplier = durabilityMultiplier;
-        this.armorVal = armorVal;
-        this.enchantability = enchantability;
-        this.equipSound = equipSound;
-        this.toughness = toughness;
-        this.knockbackResistance = knockbackResistance;
-        this.repairIngredient = repairIngredient.get();
-
-    }
-    @Override
-    public int getDurabilityForSlot(EquipmentSlot slot) {
-        return HEALTH_PER_SLOT[slot.getIndex()] * this.durabilityMultiplier;
+    SlurpiesArmorMaterials(String pName, int pDurabilityMultiplier, int[] pSlotProtections, int pEnchantmentValue, SoundEvent pSound, float pToughness, float pKnockbackResistance, Supplier<Ingredient> pRepairIngredient) {
+        this.name = pName;
+        this.durabilityMultiplier = pDurabilityMultiplier;
+        this.slotProtections = pSlotProtections;
+        this.enchantmentValue = pEnchantmentValue;
+        this.sound = pSound;
+        this.toughness = pToughness;
+        this.knockbackResistance = pKnockbackResistance;
+        this.repairIngredient = new LazyLoadedValue<>(pRepairIngredient);
     }
 
-    @Override
-    public int getDefenseForSlot(EquipmentSlot slot) {
-        return this.armorVal[slot.getIndex()];
+    public int getDurabilityForSlot(EquipmentSlot pSlot) {
+        return HEALTH_PER_SLOT[pSlot.getIndex()] * this.durabilityMultiplier;
     }
 
-    @Override
+    public int getDefenseForSlot(EquipmentSlot pSlot) {
+        return this.slotProtections[pSlot.getIndex()];
+    }
+
     public int getEnchantmentValue() {
-        return enchantability;
+        return this.enchantmentValue;
     }
 
-    @Override
     public SoundEvent getEquipSound() {
-        return equipSound;
+        return this.sound;
     }
 
-    @Override
     public Ingredient getRepairIngredient() {
-        return repairIngredient;
+        return this.repairIngredient.get();
     }
 
-    @Override
     public String getName() {
-        return name;
+        return this.name;
     }
 
-    @Override
     public float getToughness() {
-        return toughness;
+        return this.toughness;
     }
 
-    @Override
+    /**
+     * Gets the percentage of knockback resistance provided by armor of the material.
+     */
     public float getKnockbackResistance() {
-        return knockbackResistance;
+        return this.knockbackResistance;
     }
 }
